27bea4b568445275fa0520d0da189ca3c7f5eb4e
Gaston <Gaston@DESKTOP-Q182OMJ>
2024-04-04 15:29:33 -0300

    Ejercicio 1 - Gutierrez Yañez Gaston

---
+++ src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,18 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int num1=8;
+		int num2=5;
+		int num3=1;
+		int num4=20;
+		int num5=9;
+		double resultado;
+		resultado=num1+num2+num3+num4+num5;
+		resultado=resultado/5;
+		System.out.println(num1+", "+num2+", "+num3+", "+num4+", "+num5);
+		System.out.println("El promedio es: "+resultado);
+	}
+
+}
