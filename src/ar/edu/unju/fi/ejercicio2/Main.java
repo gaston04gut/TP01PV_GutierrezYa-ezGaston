f4957e2e54e1a43f20b6a1e3146a288314b5a741
Gaston <Gaston@DESKTOP-Q182OMJ>
2024-04-04 15:40:19 -0300

    Ejercicio 2 - Gutierrez Yañez Gaston

---
+++ src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,21 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String nombrePais="Argentina";
+		int edadPersona=19;
+		float alturaEdificio=(float) 451.15;
+		float precioProducto=(float) 1499.99;
+		long numeroTelefono= 3885429854l;
+		double coseno=0.9999619231;
+		
+		System.out.println("Nombre de Pais: "+nombrePais);
+		System.out.println("Edad de una Persona: "+edadPersona);
+		System.out.println("Altura de un Edificio: "+alturaEdificio);
+		System.out.println("Precio de un Producto de Supermecado: "+precioProducto);
+		System.out.println("Numero de Telefono: "+numeroTelefono);
+		System.out.println("Coseno de 0.5 = "+coseno);
+	}
+
+}
