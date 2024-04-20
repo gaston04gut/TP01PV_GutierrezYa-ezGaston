876daad8c0c4f3393ef483923bd5cffd042ae20a
Gaston <Gaston@DESKTOP-Q182OMJ>
2024-04-20 00:06:35 -0300

    Ejercicio 11 - Gutierrez Yañez

---
+++ src/ar/edu/unju/fi/ejercicio11/Main.java
@@ -0,0 +1,15 @@
+package ar.edu.unju.fi.ejercicio11;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int i=2;
+		int j;
+		int x;
+		for(j=40;j>0;j--) {
+			x=j*i;
+			i++;
+			System.out.print(x+" ");
+		}
+	}
+}
